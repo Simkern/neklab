1907f2a7473b41e9dfe1ac63dc93c8afa9580e8d
Simon KERN <johann-simon.kern@ensam.eu>
2025-01-16 12:38:08 +0100

    fixed torus steady system wrt nsteps

--- src/systems/fixed_point_torus.f90
+++ src/systems/fixed_point_torus.f90
@@ -13,17 +13,16 @@
                call vec2nek(vx, vy, vz, pr, t, vec_in)
       ! Set appropriate tolerances and Nek status
                call setup_nonlinear_solver(recompute_dt = .true., 
-     $                                     cfl_limit   = 0.4_dp,
      $                                     endtime     = pd,
+     $                                     cfl_limit   = 0.4_dp,
      $                                     vtol        = atol*0.1, 
      $                                     ptol        = atol*0.1)
       ! Intgrate the nonlinear equations forward
                time = 0.0_dp
-               call pipe%set_2d_mode('newton')       ! reset output counter to overwrite output files, compute ubar_lag
+               call pipe%reset_mflow_fft()
                do istep = 1, nsteps
                   call pipe%compute_bf_forcing(time) ! --> set neklab_forcing data
                   call nek_advance()
-                  call pipe%save_2d_fields(vx,vy,vz) ! outposts automatically at lastep == 1
                   call pipe%compute_mflow_fft(period=pd) ! integrate Fourier coefficients
                end do
       ! Record the mass flow rate and number of timesteps per period for subsequent linear runs
@@ -47,16 +46,15 @@
       ! Set the baseflow initial condition
                call abs_vec2nek(vx, vy, vz, pr, t, self%X)
       ! Ensure correct nek status
-               call setup_linear_solver(solve_baseflow = .false., 
+               call setup_linear_solver(solve_baseflow = .false.,
+     $                                  recompute_dt   = .false.,
      $                                  vtol           = atol*0.5, 
      $                                  ptol           = atol*0.5)
       ! Set the initial condition for Nek5000's linearized solver.
                call vec2nek(vxp, vyp, vzp, prp, tp, vec_in)
       ! Integrate the equations forward in time.
                time = 0.0_dp
-               call pipe%set_2d_mode('newton')         ! reset output counter to load baseflow files in order
                do istep = 1, nsteps
-                  call pipe%set_baseflow(vx, vy, vz, istep) ! sets the baseflow field and the appropriate timestep
                   call nek_advance()
                end do
       ! Extract the final solution to vector.
@@ -81,15 +79,14 @@
       ! Ensure correct nek status
                call setup_linear_solver(transpose      = .true., 
      $                                  solve_baseflow = .false.,
+     $                                  recompute_dt   = .false.,
      $                                  vtol           = atol*0.5, 
      $                                  ptol           = atol*0.5)
       ! Set the initial condition for Nek5000's linearized solver.
                call vec2nek(vxp, vyp, vzp, prp, tp, vec_in)
       ! Integrate the equations forward in time.
                time = 0.0_dp
-               call pipe%set_2d_mode('newton')         ! reset output counter to load baseflow files in order
                do istep = 1, nsteps
-                  call pipe%set_baseflow(vx, vy, vz, istep) ! sets the baseflow field and the appropriate timestep
                   call nek_advance()
                end do
       ! Extract the final solution to vector.

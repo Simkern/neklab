db5a73b35a04e62e35820153ff18e27472775291
Simon KERN <johann-simon.kern@ensam.eu>
2025-01-25 00:42:12 +0100

    improved logging

--- src/helix/helix_2d.f90
+++ src/helix/helix_2d.f90
@@ -112,13 +112,13 @@
             if (self%n2d /= self%nelf) then
                call nek_stop_error('Inconsistent elements in 2D mesh!', module=this_module, procedure='init_2d_geom')
             else
-               call nek_log_message('Global 2D element ownership', this_module, 'init_2d_geom')
+               call nek_log_message('global 2D element ownership establised', this_module, 'init_2d_geom')
                msg = 'neklab_helix % init_2d_geom :'
                do ie = 0, np-1
                   if (nid == ie) print '(A,4X,A,I3,A,I3,A)', trim(msg), 'proc ', ie, ': ', self%n2d_gown, ' 2D elements'
                   call nekgsync()
                end do
-               write(msg,'(A,I3,A)') 'Total: ', self%n2d, ' 2D elements'
+               write(msg,'(A,I3,A)') 'total: ', self%n2d, ' 2D elements'
                call nek_log_message(msg, this_module, 'init_2d_geom')
             end if
             if (debug) then

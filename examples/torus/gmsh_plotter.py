339cafdc21993be153aad7eea8a11b9bc6778290
Simon KERN <johann-simon.kern@ensam.eu>
2025-01-30 00:06:46 +0100

    cleanup, updated to new setup

--- examples/torus/gmsh_plotter.py
+++ examples/torus/gmsh_plotter.py
@@ -1,8 +1,8 @@
-import sys, os, re
-import copy
+import sys, re
 import numpy as np
-from itertools import product
 import matplotlib.pyplot as plt
+from read_2d_data import read_fields
+from plot_2d_data import plot_2d_mesh
 
 def compute_bisector(xt, yt, xb, yb):
    xm = (xt + xb)/2.0
@@ -20,7 +20,32 @@ def compute_bisector(xt, yt, xb, yb):
    ur = np.array([ ux, uy])
    return ul, ur, norm
 
-def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=False):
+def make_circle(circle, points, npoints=101):
+   p1 = points[circle[0] - 1]  # Subtract 1 for 0-indexing
+   c  = points[circle[1] - 1]    #center
+   p2 = points[circle[2] - 1]
+
+   cc  = c[0] + 1j*c[1]
+   p1c = p1[0] + 1j*p1[1]
+   p2c = p2[0] + 1j*p2[1]
+   r1  = p1c - cc
+   r2  = p2c - cc
+   da  = np.angle(r2) - np.angle(r1)
+   if (abs(da) > np.pi):
+      da = 2*np.pi - abs(da)
+   alp = np.linspace(0,da,npoints, endpoint=True)
+   circ = cc + r1*np.exp(1j*alp)
+   #print(f'circle from {np.real(circ)[0]},{np.imag(circ)[1]} to {np.real(circ)[-1]},{np.imag(circ)[-1]}')
+   return np.real(circ), np.imag(circ)
+
+def make_line(line, points):
+   p1 = points[line[0] - 1]  # Subtract 1 for 0-indexing
+   p2 = points[line[1] - 1]
+   #print(f'line from {p1[0]},{p1[1]} to {p2[0]},{p2[1]}')
+   return [p1[0], p2[0]], [p1[1], p2[1]]
+
+def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=False, extract_polygons=False):
+
    # Extract data
    R, rt, rb, RBt, RBb, tht, thb, lambda1t, lambda1b, lambda2, dyc = geom.values()
 
@@ -92,70 +117,58 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
       print(f'ulB = {ulB}')
       print(f'LB  = {LB}')
 
-   points_aux2 = np.array([
-         mr,
-         ml,
-         mrB,
-         mlB
-   ])
+   points_aux2 = np.array([  mr,  ml,  mrB,  mlB ])
 
-   points_aux = np.array([
-            [0, 0],
-            ml + L*ul,
-            [0, -lRt],
-            mr + L*ur,
-            [0, lRb],
-            [0, -dyc],
-            mlB + LB*ulB,
-            mrB + LB*urB
+   points_aux = np.array([  
+      [0, 0],
+      ml + L*ul,
+      [0, -lRt],
+      mr + L*ur,
+      [0, lRb],
+      [0, -dyc],
+      mlB + LB*ulB,
+      mrB + LB*urB
    ])
    naux = len(points_aux)
 
    # Block vertices
-   if (half):
-    points_block = np.array([
-        [dxt, dyt],
-        [dxb, -dyb],
-        [0.0, -Dyxb],
-        [0.0, Dyxt],
-        [dxBt, dyBt],
-        [dxBb, -dyBb],
-        [0.0, -RBb],
-        [0.0, RBC],
-        [Dxt, Dyt],
-        [Dxb, -Dyb],
-        [0.0, -R],
-        [0.0, R]
-    ])
-   else:
-    points_block = np.array([
-      [ dxt,   dyt],
-      [ dxb,  -dyb],
-      [-dxb,  -dyb],
-      [-dxt,   dyt],
-      [ dxBt,  dyBt],
-      [ dxBb, -dyBb],
-      [-dxBb, -dyBb],
-      [-dxBt,  dyBt],
-      [ Dxt,   Dyt],
-      [ Dxb,  -Dyb],
-      [-Dxb,  -Dyb],
-      [-Dxt,   Dyt]
+   points = np.array([
+      [  0.0, Dyxt], 
+      [  dxt,  dyt], 
+      [  dxb, -dyb], 
+      [  0.0,-Dyxb], 
+      [  0.0,  RBC], 
+      [ dxBt, dyBt], 
+      [ dxBb,-dyBb], 
+      [  0.0, -RBb], 
+      [  0.0,    R], 
+      [  Dxt,  Dyt], 
+      [  Dxb, -Dyb], 
+      [  0.0,   -R]
    ])
-    nblk = len(points_block)
+   npts = len(points)
 
-   points = np.concatenate([points_aux, points_block], axis=0)
+   points_sym = np.array([
+      [ -dxb, -dyb], 
+      [ -dxt,  dyt], 
+      [-dxBb,-dyBb], 
+      [-dxBt, dyBt], 
+      [ -Dxb, -Dyb], 
+      [ -Dxt,  Dyt]
+   ])
+
+   pts = np.concatenate([points, points_aux, points_sym], axis=0)
 
    b0 = [ 
-         points_aux2[0],
-         points_aux2[2]
-      ]
+      points_aux2[0],
+      points_aux2[2]
+   ]
    l0 = [ 
-         points_block[0],
-         points_block[1],
-         points_block[4],
-         points_block[5],
-      ]
+      points[2-1],
+      points[3-1],
+      points[6-1],
+      points[7-1],
+   ]
    
    if not half:
       b0 += [
@@ -163,10 +176,10 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
          points_aux2[3]
       ]
       l0 += [
-         points_block[2],
-         points_block[3],
-         points_block[6],
-         points_block[7]
+         points_sym[21-naux-npts-1],
+         points_sym[22-naux-npts-1],
+         points_sym[25-naux-npts-1],
+         points_sym[26-naux-npts-1]
       ]
 
    b1 = [ points_aux[3], points_aux[6] ]
@@ -181,11 +194,11 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
    hline1 = np.array(l1)
 
    if print_pts:
-      p = -points_block[0]
+      p = -points[1]
       pc = (p[0] +1j*p[1])/np.linalg.norm(p)
-      p = points_block[0] - points_aux[3]
+      p = points[1] - points_aux[3]
       p1 = (p[0] +1j*p[1])/np.linalg.norm(p)
-      p = points_block[0] - points_aux[2]
+      p = points[1] - points_aux[2]
       p2 = (p[0] +1j*p[1])/np.linalg.norm(p)
       print(f'pc = {pc}')
       print(f'p1 = {p1}')
@@ -197,11 +210,11 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
       print(f'a = {np.arccos(q1)*360/(2*np.pi)}')
       print(f'b = {180-np.arccos(q2)*360/(2*np.pi)}')
 
-      p = -points_block[1]
+      p = -points[2]
       pc = (p[0] +1j*p[1])/np.linalg.norm(p)
-      p = points_block[1] - points_aux[3]
+      p = points[2] - points_aux[3]
       p1 = (p[0] +1j*p[1])/np.linalg.norm(p)
-      p = points_block[1] - points_aux[4]
+      p = points[2] - points_aux[4]
       p2 = (p[0] +1j*p[1])/np.linalg.norm(p)
       print(f'pc = {pc}')
       print(f'p1 = {p1}')
@@ -214,39 +227,33 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
       print(f'b = {180-np.arccos(q2)*360/(2*np.pi)}')
 
    # Circle connections (correspond to the given circle definitions)
-   if (half):
-    circles = [
+   circles = [
       # inner ring
-        [4 , 3, 1],
-        [1 , 4, 2],
-        [2 , 5, 3],
+      [1, 15, 2],
+      [2, 16, 3],
+      [3, 17, 4],
       # middle ring
-        [8 , 6, 5],
-        [5 , 8, 6],
-        [6 , 1, 7],
+      [5, 18, 6],
+      [6, 20, 7],
+      [7, 13, 8],
       # outer ring
-        [12, 1,  9],
-        [ 9, 1, 10],
-        [10, 1, 11]
-    ]
-   else:
-      circles = [
+      [ 9, 13, 10],
+      [10, 13, 11],
+      [11, 13, 12]
+   ]
+   if not half:
+      circles += [
       # inner ring
-        [4, 3, 1],
-        [1, 4, 2],
-        [2, 5, 3],
-        [4, 2, 3],
-      # middle ring
-        [8, 6, 5],
-        [5, 8, 6],
-        [6, 1, 7],
-        [8, 7, 7],
-      # outer ring
-        [12, 1,  9],
-        [ 9, 1, 10],
-        [10, 1, 11],
-        [12, 1, 11]
-    ]
+         [ 4, 17, 21],
+         [22, 14, 21], # flipped order for plot
+         [22, 15,  1],
+         [ 8, 13, 23],
+         [24, 19, 23], # flipped order for plot
+         [24, 18,  5],
+         [12, 13, 25],
+         [26, 13, 25], # flipped order for plot
+         [26, 13,  9]
+      ]
 
    # Line connections (correspond to the given line definitions)
    lines = [
@@ -258,10 +265,16 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
       [6, 10],
       [7, 11],
       [8, 12],
+      [ 4, 1]
    ]
-   if (half):
-      lines = np.concatenate([ lines, [[3, 4]] ], axis=0)
-
+   if not half:
+      lines += [
+         [ 21, 23 ],
+         [ 22, 24 ],
+         [ 23, 25 ],
+         [ 24, 26 ]
+      ]
+   
    # Plot the points
    fig, ax = plt.subplots(figsize=(10,8))
 
@@ -273,11 +286,19 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
          ax.text(point[0], point[1], f'{i+1}', color='blue', fontsize=12, ha='right', va='bottom')
    
    # Plot block vertices (group 2)
-      ax.scatter(points_block[:, 0], points_block[:, 1], color='red', label='Block Vertices')  
+   ax.scatter(points[:, 0], points[:, 1], color='red', label='Block Vertices')  
 
    # Annotate block vertices
-   for i, point in enumerate(points_block):
-      ax.text(point[0], point[1], f'{points_aux.shape[0] + i+1}', color='red', fontsize=12, ha='right', va='bottom')
+   for i, point in enumerate(points):
+      ax.text(point[0], point[1], f'{i+1}', color='red', fontsize=12, ha='right', va='bottom')
+
+   if not half:
+      # Plot block vertices (group 2)
+      ax.scatter(points_sym[:, 0], points_sym[:, 1], color='red')  
+
+      # Annotate block vertices
+      for i, point in enumerate(points_sym):
+         ax.text(point[0], point[1], f'{naux + points.shape[0] + i+1}', color='red', fontsize=12, ha='right', va='bottom')
 
    if aux2:
       # Plot auxiliary points 2
@@ -290,45 +311,30 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
          ax.plot([px[0], py[0]], [px[1], py[1]], c = 'black', linestyle='--')
       for i, (px, py) in enumerate(zip(hline0,hline1)):
          ax.plot([px[0], py[0]], [px[1], py[1]], c = 'black', alpha=0.25)
-
+   
    # Plot circles (connecting points according to the circle definitions)
    for cidx, circle in enumerate(circles):
-      p1 = points[circle[0] - 1 + naux]  # Subtract 1 for 0-indexing
-      c  = points[circle[1] - 1]    #center
-      p2 = points[circle[2] - 1 + naux]
-
-      cc  = c[0] + 1j*c[1]
-      p1c = p1[0] + 1j*p1[1]
-      p2c = p2[0] + 1j*p2[1]
-      r1  = p1c - cc
-      r2  = p2c - cc
-      da  = np.angle(r2) - np.angle(r1)
-      if (abs(da) > np.pi):
-         da = 2*np.pi - abs(da)
-      alp = np.linspace(0,da,101, endpoint=True)
-      circ = cc + r1*np.exp(1j*alp)
+      xcirc, ycirc = make_circle(circle, pts)
       
       # Draw the circle segments
-      #ax.plot([p1[0], p2[0]], [p1[1], p2[1]], color='green', linestyle='-', linewidth=1)
-      ax.plot(np.real(circ), np.imag(circ), color='green', linestyle='-', linewidth=1)
+      ax.plot(xcirc, ycirc, color='green', linestyle='-', linewidth=1)
          
       # Add annotation at the midpoint
-      ax.text(np.real(circ[50]), np.imag(circ[50]), f'{cidx+1}', color='green', fontsize=12, ha='left', va='bottom')
+      ax.text(xcirc[50], ycirc[50], f'{cidx+1}', color='green', fontsize=12, ha='left', va='bottom')
 
    # Plot lines (connecting points according to the line definitions)
    for lidx, line in enumerate(lines):
-      p1 = points[line[0] - 1 + naux]  # Subtract 1 for 0-indexing
-      p2 = points[line[1] - 1 + naux]
+      xp, yp = make_line(line, pts)
       
       # Draw the line connecting the two points
-      ax.plot([p1[0], p2[0]], [p1[1], p2[1]], color='purple', linestyle='-', linewidth=1)
+      ax.plot(xp, yp, color='purple', linestyle='-', linewidth=1)
 
       # Calculate the midpoint for annotation
-      midpoint = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
+      midpoint = [np.sum(xp)/2.0, np.sum(yp)/2.0]
       
       # Add annotation at the midpoint
       ax.text(midpoint[0], midpoint[1], f'{cidx+1+lidx+1}', color='purple', fontsize=12, ha='left', va='bottom')
-
+ 
    # Labels and title
    ax.set_xlabel('X')
    ax.set_ylabel('Y')
@@ -337,4 +343,3 @@ def plot_gmsh(geom, half=False, aux1=False, aux2=False, hlines=False, print_pts=
 
    # Display the plot
    plt.axis('equal')
-   plt.show()

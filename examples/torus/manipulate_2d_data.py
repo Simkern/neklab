add5c5d3544716d38eca11799b90cfcaaa9646da
Simon KERN <johann-simon.kern@ensam.eu>
2025-01-30 16:39:58 +0100

    Refactored with more checks. Symmetrization and h -> f routines updated.

--- examples/torus/manipulate_2d_data.py
+++ examples/torus/manipulate_2d_data.py
@@ -10,116 +10,195 @@ from plot_2d_data import plot_2d_mesh
 def flipud(f):
     return np.flip(f, axis=0)
 
-def antitranspose(f):
-    fld = np.empty_like(f)
-    lx1, ly1 = f.shape[:2]
-    for i in range(lx1):
-        for j in range(ly1):
-            fld[i,j,...] = f[lx1-1-j, ly1-1-i,...]
-    return fld
-
 def fliplr(f):
     return np.flip(f, axis=1)
 
-def trans(f):
+def flipudlr(f):
     return np.flip(f, axis=(0,1))
 
-def symmetrize_fld(x0, y0, fld, if_debug=False):
-    lx1, _, nelf = x0.shape
-    xa = np.mean(x0, axis=(0,1))
-    ya = np.mean(y0, axis=(0,1))
-    dx = min(np.max(x0, axis=(0,1)) - np.min(x0, axis=(0,1)))
-    dy = min(np.max(y0, axis=(0,1)) - np.min(y0, axis=(0,1)))
-    tol = (dx+dy)/10e6
+def flipat(f):
+    fld = np.empty_like(f)
+    a, b = f.shape[:2]
+    for i in range(a):
+        for j in range(b):
+            if len(f.shape)==2:
+                fld[i,j]     = f[a-1-j, b-1-i]
+            else:
+                fld[i,j,...] = f[a-1-j, b-1-i, ...]
+    return fld
 
-    mapped = [ False for i in range(nelf) ]
-    imap = 0
-    xs, ys = np.empty_like(x0), np.empty_like(x0)
-    fld_sym, fld_asym = np.empty_like(fld), np.empty_like(fld)
+def fliptr(f):
+    if len(f.shape)==2:
+        fld = f.T
+    else:
+        fld = np.empty_like(f)
+        for k in range(f.shape[2]):
+            fld[:,:,k] = f[:,:,k].T
+    return fld
 
-    for i, (x1, y1) in enumerate(zip(xa, ya)):
-        if not mapped[i]:
-            for j, (x2, y2) in enumerate(zip(xa, ya)):
-                d = np.sqrt((x1 + x2) ** 2 + (y1 - y2) ** 2)
-                if d < tol:
-                    if x1 > 0:
-                        l = i
-                        r = j
-                    else:
-                        l = j
-                        r = i
-                    idx1 = np.argsort([  x0[a,b,l]+y0[a,b,l] for (a,b) in itertools.product([0,lx1-1], repeat=2) ])
-                    idx2 = np.argsort([ -x0[a,b,r]+y0[a,b,r] for (a,b) in itertools.product([0,lx1-1], repeat=2) ])
-                    #print(idx2[idx1])
-                    s = idx2[idx1]
-                    xl   = x0[:,:,l]
-                    yl   = y0[:,:,l]
-                    fldl = fld[:,:,l,:]
-                    if (s == [1, 0, 3, 2]).all():
-                        xr   = flipud(x0[:,:,r])
-                        yr   = flipud(y0[:,:,r])
-                        fldr = flipud(fld[:,:,r,:])
-                    elif (s == [3, 1, 2, 0]).all():
-                        xr   = antitranspose(x0[:,:,r])
-                        yr   = antitranspose(y0[:,:,r])
-                        fldr = antitranspose(fld[:,:,r,:])
-                    elif (s == [2, 3, 0, 1]).all():
-                        xr   = flipud(x0[:,:,r])
-                        yr   = flipud(x0[:,:,r])
-                        fldr = flipud(fld[:,:,r,:])
-                    elif (s == [1, 2, 0, 3]).all():
-                        xr   = flipud(x0[:,:,r])
-                        yr   = flipud(y0[:,:,r])
-                        fldr = flipud(fld[:,:,r,:])
+def print_msg(typestr, x1, xt, err):
+    print(f'\tx:',' '.join([ f'{d:8.4f}' for d in x1[0,:] ]),' --> ',' '.join([ f'{d:8.4f}' for d in xt[0,:] ]))
+    print(f'\t  ',' '.join([ f'{d:8.4f}' for d in x1[1,:] ]),' --> ',' '.join([ f'{d:8.4f}' for d in xt[1,:] ]))
+    print(f'error after {typestr}: {err}')
 
-                    err = abs((xl+yl+xr-yr).max())
-                    if err>0.001:
-                        if (s == [2, 3, 0, 1]).all():
-                            xr   = fliplr(x0[:,:,r])
-                            yr   = fliplr(y0[:,:,r])
-                            fldr = fliplr(fld[:,:,r,:])
-                        if (s == [1, 0, 3, 2]).all():
-                            xr   = fliplr(x0[:,:,r])
-                            yr   = fliplr(y0[:,:,r])
-                            fldr = fliplr(fld[:,:,r,:])
-                        elif (s == [1, 2, 0, 3]).all():
-                            xr   = fliplr(x0[:,:,r])
-                            yr   = fliplr(y0[:,:,r])
-                            fldr = fliplr(fld[:,:,r,:])
-
-                    err = abs((xl+yl+xr-yr).max())
-                    #print(err)
-                    if (err > 0.0001):
-                        print(f'x el1:')
-                        for el in range(0,lx1,1):
-                            print(' '.join([ f' {d:8.4f}' for d in x0[el,range(0,lx1,1),l] ]))
-                        print(f'-x el2:')
-                        for el in range(0,lx1,1):
-                            print(' '.join([ f' {d:8.4f}' for d in -x0[el,range(0,lx1,1),r] ]))
-                        print(f'-xr:')
-                        for el in range(0,lx1,1):
-                            print(' '.join([ f' {d:8.4f}' for d in -xr[el,range(0,lx1,1)] ]))
-                        print('\n\n')
-                        print('\n\n')
-                        sys.exit()
-                    flds = (fldl + fldr)/2.0
-                    fld_sym[:,:,i] = flds.copy()
-                    fld_sym[:,:,j] = flds.copy()
-                    fld_asym[:,:,i] = fldl - flds
-                    fld_asym[:,:,j] = fldr - flds
-                    xs[:,:,i] = xl
-                    xs[:,:,j] = xr
-                    ys[:,:,i] = yl
-                    ys[:,:,j] = yr
-
-                    mapped[j] = True
-                    imap += 1
-            mapped[i] = True
-            imap += 1
-    if not imap == nelf:
-        print('Not all poitns mapped!')
+def get_ord(x0,y0,x1,y1,tol=1e-12,debug=False):
+    # we assume that the input data contain the flipped data reflecting on which side of the mesh the elements are located
+    s0 = x0+y0
+    err = abs(s0-(x1+y1)).max()
+    if err < tol:
+        ord = 0
+    if err > tol:
+        xtc = fliplr(x1)
+        ytc = fliplr(y1)
+        err = abs(s0-(xtc+ytc)).max()
+        if debug:
+            print_msg('lr', x1, xtc, err)
+        if err < tol:
+            ord = 1
+    if err > tol:
+        xtc = flipud(x1)
+        ytc = flipud(y1)
+        err = abs(s0-(xtc+ytc)).max()
+        if debug:
+            print_msg('ud', x1, xtc, err)
+        if err < tol:
+            ord = 2
+    if err > tol:
+        xtc = flipudlr(x1)
+        ytc = flipudlr(y1)
+        err = abs(s0-(xtc+ytc)).max()
+        if debug:
+            print_msg('udlr', x1, xtc, err)
+        if err < tol:
+            ord = 3
+    if err > tol:
+        xtc = fliptr(x1)
+        ytc = fliptr(y1)
+        err = abs(s0-(xtc+ytc)).max()
+        if debug:
+            print_msg('transpose', x1, xtc, err)
+        if err < tol:
+            ord = 4
+    if err > tol:
+        xtc = flipat(x1)
+        ytc = flipat(y1)
+        err = abs(s0-(xtc+ytc)).max()
+        if debug:
+            print_msg('anti-tranpose', x1, xtc, err)
+        if err < tol:
+            ord = 5
+    if err > tol:
+        print(f'The elements could not be mapped!')
+        print('Target:')
+        print(f'\tx:',' '.join([ f'{d:8.4f}' for d in x0[0,:] ]),' y:',' '.join([ f'{d:8.4f}' for d in y0[0,:] ]))
+        print(f'\t  ',' '.join([ f'{d:8.4f}' for d in x0[1,:] ]),'   ',' '.join([ f'{d:8.4f}' for d in y0[1,:] ]))
+        print('Origin:')
+        print(f'\tx:',' '.join([ f'{d:8.4f}' for d in x1[0,:] ]),' y:',' '.join([ f'{d:8.4f}' for d in y1[0,:] ]))
+        print(f'\t  ',' '.join([ f'{d:8.4f}' for d in x1[1,:] ]),'   ',' '.join([ f'{d:8.4f}' for d in y1[1,:] ]))
         sys.exit()
-    return xs, ys, fld_sym, fld_asym
+    return ord
+
+def flip_data(f, ord):
+    if ord == 0:
+        return f
+    elif ord == 1: # lr
+        return fliplr(f)
+    elif ord == 2: # ud
+        return flipud(f)
+    elif ord == 3: # udlr
+        return flipudlr(f)
+    elif ord == 4: # tr
+        return fliptr(f)
+    elif ord == 5: # at
+        return flipat(f)
+    else:
+        print(f'Inconsistent value for ord: {ord}')
+        sys.exit()
+
+def get_corners(x,y):
+    if x.shape == y.shape:
+        if len(x.shape)==2:
+            lx1, ly1 = x.shape
+            xc = x[[0, 0, lx1-1, lx1-1], [0, ly1-1, 0, ly1-1]].reshape(2,2)
+            yc = y[[0, 0, lx1-1, lx1-1], [0, ly1-1, 0, ly1-1]].reshape(2,2)
+        elif len(x.shape)==3:
+            lx1, ly1, nel = x.shape
+            xc = x[[0, 0, lx1-1, lx1-1], [0, ly1-1, 0, ly1-1], :].reshape(2,2,nel)
+            yc = y[[0, 0, lx1-1, lx1-1], [0, ly1-1, 0, ly1-1], :].reshape(2,2,nel)
+        else:
+            print('Input data has the wrong size!')
+            print(f'x: {x.shape}')
+            print(f'y: {y.shape}')
+            sys.exit()
+    else:
+        print('Input data has unequal size!')
+        print(f'x: {x.shape}')
+        print(f'y: {y.shape}')
+        sys.exit()
+    return xc, yc
+
+def symmetrize_fld(dataf, fld, tol=1e-6, debug=False):
+    lx1, ly1, nelf = dataf.x.shape
+
+    xs, ys = np.empty_like(dataf.x), np.empty_like(dataf.y)
+    fld_s, fld_a = np.empty_like(fld), np.empty_like(fld)
+
+    # get element centers
+    xa = np.mean(dataf.x, axis=(0,1))
+    ya = np.mean(dataf.y, axis=(0,1))
+
+    # extract corners for the entire mesh
+    xc, yc = get_corners(dataf.x, dataf.y)
+
+    # find elements on each side (no cell center can be at 0.0)
+    il = [ i for i in range(nelf) if xa[i] < 0.0 ]
+    ir = [ i for i in range(nelf) if xa[i] > 0.0 ]
+    nl = len(il)
+    nr = len(ir)
+    if not (nl == nr == int(nelf/2.0)):
+        print('Inconsistent mesh split:')
+        print(f'Left side:  {nl} elements.')
+        print(f'Right side: {nr} elements.')
+        print(f'Total: {nelf} elements.')
+        sys.exit()
+
+    imap = 0
+    mapped = []
+    # map elements to left side
+    for i in il:
+        xl, yl, xlc, ylc, fl = xa[i], ya[i], xc[:,:,i], yc[:,:,i], fld[:,:,i,:]
+        found = False
+        mindist = 1000.0
+        for j in ir:
+            xr, yr, xrc, yrc, fr = -xa[j], ya[j], -xc[:,:,j], yc[:,:,j], fld[:,:,j,:]
+            d = np.sqrt((xl - xr) ** 2 + (yl - yr) ** 2)
+            if d < mindist:
+                mindist = d
+                imin = j
+                x_ = xr
+                y_ = yr
+            if d < tol:
+                order = get_ord(xlc, ylc, xrc, yrc, tol=tol)
+                # apply
+                fr_l = flip_data(fr, order)
+                fs   = (fl + fr_l)/2.0
+                fld_s[:,:,i,:] = fs.copy()
+                fld_s[:,:,j,:] = flip_data(fs, order)
+                fld_a[:,:,i,:] = fl - fld_s[:,:,i,:]
+                fld_a[:,:,j,:] = fr - fld_s[:,:,j,:]
+                mapped.append([i,j])
+                imap += 1
+                found = True
+        if not found:
+            print(f'\nNo match found for element {i} with tolerance {tol:16.12f}')
+            print(f'\t centroid: {xl:16.12f}, {yl:16.12f}')
+            print(f'Closest match: \n  element {imin} with d = {mindist:16.12f}')
+            print(f'\t centroid: {x_:16.12f}, {y_:16.12f}')
+            sys.exit()
+    if not 2*imap == nelf:
+        print(f'imap = {imap}')
+        sys.exit()
+    return fld_s, fld_a
 
 def link_meshes(x0h, y0h, x0f, y0f, pattern=None):
     
@@ -184,115 +263,38 @@ def link_meshes(x0h, y0h, x0f, y0f, pattern=None):
 
 def h2d_to_f2d(h2d_pattern, hfldr, f2d_file_ref, ffldr):
 
-    xh, yh, vxh, vyh, vzh, _, dt2d, _, nsteps = read_fields(h2d_pattern, cwd=hfldr)
-    xf, yf, _, _, _,       elmapf, _,    metaf = read_binary_file(f2d_file_ref, only_mesh=True)
+    datah, metah, nsteps  = read_fields(h2d_pattern, cwd=hfldr)
+    dataf, metaf          = read_binary_file(f2d_file_ref, only_mesh=True)
 
-    map_h2f, _ = link_meshes(xh, yh, xf, yf)
+    map_h2f, _ = link_meshes(datah.x, datah.y, dataf.x, dataf.y)
 
-    lx1, ly1, nelfh = vxh.shape[:3]
+    lx1, ly1, nelfh = datah.vx.shape[:3]
     shp = [lx1, ly1, 2*nelfh, nsteps]
     vx, vy, vz = np.empty(shp), np.empty(shp), np.empty(shp)
 
-    sv = [1, -1]
+    xhc, yhc = get_corners(datah.x, datah.y) # extract corners for the entire mesh
+    xfc, yfc = get_corners(dataf.x, dataf.y) # extract corners for the entire mesh
+
+    orientation = [ ]
+    sv = [1, -1] # we assume that the first element in each pair in map_h2f is on the left side
     for i, idx in enumerate(map_h2f):
-        idx1 = np.argsort([  xh[a,b,i]+yh[a,b,i] for (a,b) in itertools.product([0,lx1-1], repeat=2) ])
-        #print(xh[0,:,i])
-        #print(f'\nhel {i:3d}: \t{idx1}')
-        x0 = xh[:,:,i]
-        y0 = yh[:,:,i]
+        x0, y0 = xhc[:,:,i], yhc[:,:,i]
+        ord = []
         for id, j in enumerate(idx):
-            flip = sv[id]
-            idx2 = np.argsort([ flip*xf[a,b,j]+yf[a,b,j] for (a,b) in itertools.product([0,lx1-1], repeat=2) ])
-            if (idx1 == idx2).all():
-                xt = xf[:,:,j]
-                yt = yf[:,:,j]
-                vx[:,:,j,:] = vxh[:,:,i,:]
-                vy[:,:,j,:] = vyh[:,:,i,:]
-                vz[:,:,j,:] = vzh[:,:,i,:]
-            else:
-                s = idx1[idx2]
-                if (s == [0, 1, 2, 3]).all():
-                    xt          = xf[:,:,j]
-                    yt          = yf[:,:,j]
-                    vx[:,:,j,:] = vxh[:,:,i,:]
-                    vy[:,:,j,:] = vyh[:,:,i,:]
-                    vz[:,:,j,:] = vzh[:,:,i,:]
-                elif (s == [3, 2, 1, 0]).all():
-                    xt          = trans(xf[:,:,j])
-                    yt          = trans(yf[:,:,j])
-                    vx[:,:,j,:] = trans(vxh[:,:,i,:])
-                    vy[:,:,j,:] = trans(vyh[:,:,i,:])
-                    vz[:,:,j,:] = trans(vzh[:,:,i,:])
-                elif (s == [3, 1, 2, 0]).all():
-                    xt          = antitranspose(xf[:,:,j])
-                    yt          = antitranspose(yf[:,:,j])
-                    vx[:,:,j,:] = antitranspose(vxh[:,:,i,:])
-                    vy[:,:,j,:] = antitranspose(vyh[:,:,i,:])
-                    vz[:,:,j,:] = antitranspose(vzh[:,:,i,:])
-                elif (s == [1, 0, 3, 2]).all():
-                    xt          = fliplr(xf[:,:,j])
-                    yt          = fliplr(yf[:,:,j])
-                    vx[:,:,j,:] = fliplr(vxh[:,:,i,:])
-                    vy[:,:,j,:] = fliplr(vyh[:,:,i,:])
-                    vz[:,:,j,:] = fliplr(vzh[:,:,i,:])
-                elif (s == [0, 2, 1, 3]).all():
-                    xt              = xf[:,:,j].T
-                    yt              = yf[:,:,j].T
-                    for l in range(vx.shape[-1]):
-                        vx[:,:,j,l] = vxh[:,:,i,l].T
-                        vy[:,:,j,l] = vyh[:,:,i,l].T
-                        vz[:,:,j,l] = vzh[:,:,i,l].T
-            err = abs((x0+y0-flip*xt-yt).max())
-            if (err > 0.0001):
-                if (s == [3, 1, 2, 0]).all():
-                    xt              = xf[:,:,j].T
-                    yt              = yf[:,:,j].T
-                    for l in range(vx.shape[-1]):
-                        vx[:,:,j,l] = vxh[:,:,i,l].T
-                        vy[:,:,j,l] = vyh[:,:,i,l].T
-                        vz[:,:,j,l] = vzh[:,:,i,l].T
-                elif (s == [0, 3, 2, 1]).all():
-                    xt              = xf[:,:,j].T
-                    yt              = yf[:,:,j].T
-                    for l in range(vx.shape[-1]):
-                        vx[:,:,j,l] = vxh[:,:,i,l].T
-                        vy[:,:,j,l] = vyh[:,:,i,l].T
-                        vz[:,:,j,l] = vzh[:,:,i,l].T
-                elif (s == [0, 1, 2, 3]).all():
-                    xt          = trans(xf[:,:,j])
-                    yt          = trans(yf[:,:,j])
-                    vx[:,:,j,:] = trans(vxh[:,:,i,:])
-                    vy[:,:,j,:] = trans(vyh[:,:,i,:])
-                    vz[:,:,j,:] = trans(vzh[:,:,i,:])
-                elif (s == [2, 1, 3, 0]).all():
-                    xt          = trans(xf[:,:,j])
-                    yt          = trans(yf[:,:,j])
-                    vx[:,:,j,:] = trans(vxh[:,:,i,:])
-                    vy[:,:,j,:] = trans(vyh[:,:,i,:])
-                    vz[:,:,j,:] = trans(vzh[:,:,i,:])
-                elif (s == [0, 2, 1, 3]).all():
-                    xt          = antitranspose(xf[:,:,j])
-                    yt          = antitranspose(yf[:,:,j])
-                    vx[:,:,j,:] = antitranspose(vxh[:,:,i,:])
-                    vy[:,:,j,:] = antitranspose(vyh[:,:,i,:])
-                    vz[:,:,j,:] = antitranspose(vzh[:,:,i,:])
-                err = abs((x0+y0-flip*xt-yt).max())
-                if (err > 0.0001):
-                    print(f'Error = {err}')
-                    print(f'       : \t', end='')
-                    print(' '.join([ f'{d:8.4f}' for d in [ xh[x,y,i] for (x,y) in zip([0,0,lx1-1,lx1-1],[0,lx1-1,0,lx1-1])] ]))
-                    print(f'       : \t', end='')
-                    print(' '.join([ f'{d:8.4f}' for d in [ yh[x,y,i] for (x,y) in zip([0,0,lx1-1,lx1-1],[0,lx1-1,0,lx1-1])] ]))
-                    print(s)
-                    print(f'\tfel {j:3d}:')
-                    print(f'\t       :', end='')
-                    print(' '.join([ f'{d:8.4f}' for d in [ flip*xf[x,y,j] for (x,y) in zip([0,0,lx1-1,lx1-1],[0,lx1-1,0,lx1-1])] ]))
-                    print(f'\t       :', end='')
-                    print(' '.join([ f'{d:8.4f}' for d in [ yf[x,y,j] for (x,y) in zip([0,0,lx1-1,lx1-1],[0,lx1-1,0,lx1-1])] ]))
-                    print(f'\tflip{j:3d}:', end='')
-                    print(' '.join([ f'{d:8.4f}' for d in [ flip*xt[x,y] for (x,y) in zip([0,0,lx1-1,lx1-1],[0,lx1-1,0,lx1-1])] ]))
-                    print(f'\tflip{j:3d}:', end='')
-                    print(' '.join([ f'{d:8.4f}' for d in [ yt[x,y] for (x,y) in zip([0,0,lx1-1,lx1-1],[0,lx1-1,0,lx1-1])] ]))
-                    sys.exit()
-    write_fields(h2d_pattern+'_f', xf, yf, vx, vy, vz, elmapf, dt2d, metaf, nsteps, cwd=ffldr, force=True)
+            flip = sv[id] # flip the x-axis if the element is on the right side
+            x1, y1 = flip*xfc[:,:,j], yfc[:,:,j]
+            # get orientation
+            order = get_ord(x0, y0, x1, y1, tol=1e-12)
+            # apply
+            vx[:,:,j,:] = flip_data(datah.vx[:,:,i,:], order)
+            vy[:,:,j,:] = flip_data(datah.vy[:,:,i,:], order)
+            vz[:,:,j,:] = flip_data(datah.vz[:,:,i,:], order)
+            ord.append(order)
+        orientation.append(ord)
+    dataf.vx = vx
+    dataf.vy = vy
+    dataf.vz = vz
+    dataf.dt = datah.dt
+    # write file
+    write_fields(h2d_pattern+'_f', dataf, metaf, nsteps, cwd=ffldr, force=True)
 

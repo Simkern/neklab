840ab69773c161a45f890e2591516cf22a4cced8
Simon KERN <johann-simon.kern@ensam.eu>
2025-10-17 14:02:45 +0200

    add pressure vector and operator

---
+++ src/linops/pressure_projection_operator.f90
@@ -0,0 +1,77 @@
+      submodule(neklab_linops) pressure_projection_operator
+         implicit none
+      contains
+         module procedure pressure_projection_init
+         ! internal
+         character(len=*), parameter :: this_procedure = 'pressure_projection_init'
+         character(len=128) :: msg
+         integer :: ntot
+         ntot = lx1*ly1*lz1*nelv
+         self%baseflow         = bf
+         self%betaz            = betaz
+         self%integration_type = integration_type
+         call sethlm(self%h1,self%h2,self%integration_type)
+         call copy(self%h2inv, h2inv, ntot)
+         self%is_initialized = .true.
+         write(msg,'(A,I6)')'Setup complete for intype =', integration_type
+         call nek_log_message(msg, this_module, this_procedure)
+         end procedure pressure_projection_init
+      
+         module procedure pressure_projection_matvec
+         ! internal
+         character(len=*), parameter :: this_procedure = 'pressure_projection_matvec'
+         real(dp), dimension(lx1*ly1*lz1) :: wrk1, wrk2
+         real(dp), dimension(lx1,ly1,lz1,lelv) :: wdivm1
+         real(dp), dimension(lx2,ly2,lz2,lelv) :: wdivm2
+         integer :: ie, ntot1, ntot2
+         ntot1 = lx1*ly1*lz1*nelv
+         ntot2 = lx2*ly2*lz2*nelv
+         select type (vec_in)
+         type is (nekp_dvector)
+            select type (vec_out)
+            type is (nekp_dvector)
+               if (.not.self%is_initialized) then
+                  call nek_stop_error('pressure_projection operator not set up.', this_module, this_procedure)
+               end if
+               ! Regular Ax = D (h2*B)^(-1) D^T
+               call cdabdtp(vec_out%pr, vec_in%pr, self%h1, self%h2, self%h2inv, self%integration_type)
+               ! add 3rd perturbation component. 
+               ! we want to subtract the contribution
+               !
+               ! B * d/dz (h2*B)^(-1) d/dz wp = (-i betaz) (h2)^(-1) (-i betaz) wp = - betaz^2 (h2)^(-1) wp
+               !
+               ! Note: we are only considering the ifanls = .false. case
+               call mappr  (wdivm1, vec_in%pr, wrk1, wrk2)             ! map to vmesh
+               call col2   (wdivm1, v3mask, ntot1)
+               call col2c  (wdivm1, self%h2inv, -self%betaz**2, ntot1) ! collate -betaz^2 * (h2)^-1
+               call dssum  (wdivm1, lx1, ly1, lz1)                     ! make continuous
+               do ie = 1, nelv
+                  call map12 (wdivm2(1,1,1,ie), wdivm1(1,1,1,ie), ie)  ! map wdiv to pmesh
+               end do
+               call col2(wdivm2, bm2, ntot2)
+               call sub2(vec_out%pr, wdivm2, ntot2)
+            class default
+               call type_error('vec_out','nekp_dvector','OUT',this_module,this_procedure)
+            end select
+         class default
+            call type_error('vec_in','nekp_dvector','IN',this_module,this_procedure)
+         end select
+         end procedure pressure_projection_matvec
+   
+         !module procedure apply_hsmg_preconditioner
+         !! internal
+         !character(len=*), parameter :: this_procedure = 'apply_hsmg_preconditioner'  
+         !integer :: ntot
+         !ntot = lx1*ly1*lz1*nelv
+         !select type (vec)
+         !type is (nekp_dvector)
+         !   if (.not.self%is_initialized) then
+         !      call nek_stop_error('Preconditioner not set up.', this_module, this_procedure)
+         !   end if
+         !   call col2(vec%v,self%D,ntot)
+         !   !call copy(vec%v,tmp,ntot)
+         !class default
+         !   call type_error('vec','nekv_dvector','INOUT',this_module,this_procedure)
+         !end select
+         !end procedure apply_hsmg_preconditioner
+      end submodule
---
+++ src/vectors/pressure_component.f90
@@ -0,0 +1,75 @@
+      submodule(neklab_vectors) pressure_component
+         implicit none
+      contains
+      
+      !-------------------------------
+      !-----     CONSTRUCTOR     -----
+      !-------------------------------
+      
+         module procedure construct_nekp_dvector
+      ! Pressure.
+         out%pr = pr
+         end procedure
+      
+      !-----------------------------------------
+      !-----     TYPE-BOUND PROCEDURES     -----
+      !-----------------------------------------
+      
+         module procedure nekp_dzero
+         call self%scal(0.0_dp)
+         end procedure
+      
+         module procedure nekp_drand
+         logical :: normalize
+         integer :: i, ieg, iel
+         real(kind=dp) :: xl(ldim), fcoeff(3), alpha
+         normalize = optval(ifnorm, .false.)
+         do i = 1, lp
+            ieg = lglel(iel)
+            xl(1) = xm2(i, 1, 1, 1)
+            xl(2) = ym2(i, 1, 1, 1)
+            if (if3D) xl(ldim) = zm2(i, 1, 1, 1)
+            call random_number(fcoeff); fcoeff = fcoeff*1.0e4_dp
+            self%pr(i) = self%pr(i) + mth_rand(i, 1, 1, 1, xl, fcoeff)
+         end do
+         if (normalize) then
+            alpha = self%norm()
+            call self%scal(1.0_dp/alpha)
+         end if
+         end procedure
+      
+         module procedure nekp_dscal
+         call dscal(lp, alpha, self%pr, 1)
+         end procedure
+      
+         module procedure nekp_daxpby
+         integer :: n2
+         n2 = nx2*ny2*nz2*nelv
+         call self%scal(beta)
+         select type (vec)
+         type is (nekp_dvector)
+            call add2s2(self%pr, vec%pr, alpha, n2)
+         class default
+            call type_error('vec','nekp_dvector','IN',this_module,'nekp_daxpby')
+         end select
+         end procedure
+      
+         module procedure nekp_ddot
+         real(kind=dp), external :: glsc2
+         integer :: n
+         n = nx2*ny2*nz2*nelv
+         select type (vec)
+         type is (nekp_dvector)
+            !alpha = glsc3(self%pr, vec%pr, bm2, lp)
+            !alpha = glsc3(self%pr, bm2inv, vec%pr, lp)/volvm2    ! rnorm from convprn in navier1.f
+            alpha = glsc2(self%pr, vec%pr, n)                    ! convprn in navier1.f
+         class default
+            call type_error('vec','nekp_dvector','IN',this_module,'nekp_ddot')
+         end select
+         end procedure
+      
+         module procedure nekp_dsize
+         N = nx2*ny2*nz2*nelv
+         end procedure
+      
+      end submodule

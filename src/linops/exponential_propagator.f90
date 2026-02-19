3bf6d1942432ace6a1b80fa301515743721f47da
Simon KERN <johann-simon.kern@ensam.eu>
2026-02-19 14:34:44 +0100

    Refactoring, ensure BF defined before setup_solver. Added restarts.

--- src/linops/exponential_propagator.f90
+++ src/linops/exponential_propagator.f90
@@ -2,10 +2,10 @@
          implicit none
       contains
          module procedure init_exptA
-         ! For the baseflow field for dt/nsteps/cfl computation.
+      ! For the baseflow field for dt/nsteps/cfl computation.
          call vec2nek(vx, vy, vz, pr, t, self%baseflow)
-         call nek_log_message("Set self%baseflow -> vx, vy, vz, pr, t", this_module, "init_exptA")
-         ! Setup Nek5000 for perturbation solver.
+         call nek_log_information("Set self%baseflow -> vx, vy, vz, pr, t", this_module, "init_exptA")
+      ! Setup Nek5000 for perturbation solver.
          call setup_linear_solver(solve_baseflow = .false.,
      &                            endtime        = self%tau,
      &                            recompute_dt   = .true.,
@@ -13,60 +13,131 @@
          end procedure
 
          module procedure exptA_matvec
+         character(len=*), parameter :: this_procedure = 'exptA_matvec'
+         integer :: nrst
          select type (vec_in)
          type is (nek_dvector)
             select type (vec_out)
             type is (nek_dvector)
-               call setup_linear_solver(transpose     = .false.,
-     &                                  silent        = .false.,
-     &                                  endtime       = self%tau,
-     &                                  recompute_dt  = .true.,
-     &                                  cfl_limit     = 0.5_dp)
-         ! Force baseflow.
+
+               nrst = abs(param(27)) - 1
+      ! Set baseflow.
                call vec2nek(vx, vy, vz, pr, t, self%baseflow)
-         ! Set initial condition for the linearized solver.
+      
+      ! Set nek configuration (after the v[xzy] and v[xyz]p fields are updated)   
+               call setup_linear_solver(transpose    = .false.,
+     &                                  silent       = .true.,
+     &                                  endtime      = self%tau,
+     &                                  recompute_dt = .true.,
+     &                                  cfl_limit    = 0.5_dp)
+
+      ! Set initial condition for the linearized solver.
                call vec2nek(vxp, vyp, vzp, prp, tp, vec_in)
-         ! Integrate the equations forward in time.
+      
+      ! Integrate the equations forward in time.
                time = 0.0_dp
                do istep = 1, nsteps
+
                   call nek_advance()
+                  
+                  ! Set restart fields if present.
+                  if (istep <= nrst) call self%get_rst(vec_in, istep)
+
                end do
-         ! Copy the final solution to vector.
+      
+      ! Copy the final solution to vector.
                call nek2vec(vec_out, vxp, vyp, vzp, prp, tp)
+      
+      ! Compute restart fields.
+               call self%compute_rst(vec_out, nrst)
+
             class default
-               call type_error('vec_out','nek_dvector','OUT',this_module,'exptA_matvec')
+               call type_error('vec_out','nek_dvector','OUT',this_module, this_procedure)
             end select
          class default
-            call type_error('vec_in','nek_dvector','IN',this_module,'exptA_matvec')
+            call type_error('vec_in','nek_dvector','IN',this_module, this_procedure)
          end select
          end procedure
 
          module procedure exptA_rmatvec
+         character(len=*), parameter :: this_procedure = 'exptA_rmatvec'
+         integer :: nrst
          select type (vec_in)
          type is (nek_dvector)
             select type (vec_out)
             type is (nek_dvector)
-               call setup_linear_solver(transpose     = .true.,
-     &                                  silent        = .false.,
-     &                                  endtime       = self%tau,
-     &                                  recompute_dt  = .true.,
-     &                                  cfl_limit     = 0.5_dp)
-         ! Force baseflow.
+            
+               nrst = abs(param(27)) - 1
+      ! Set baseflow.
                call vec2nek(vx, vy, vz, pr, t, self%baseflow)
-         ! Set initial condition for the linearized solver.
+      
+      ! Set nek configuration (after the v[xzy] and v[xyz]p fields are updated)
+               call setup_linear_solver(transpose    = .true., 
+     &                                  silent       = .true.,  
+     &                                  endtime      = self%tau,  
+     &                                  recompute_dt = .true.,  
+     &                                  cfl_limit    = 0.5_dp)
+
+      ! Set initial condition for the linearized solver.
                call vec2nek(vxp, vyp, vzp, prp, tp, vec_in)
-         ! Integrate the equations forward in time.
+      
+      ! Integrate the equations forward in time.
                time = 0.0_dp
                do istep = 1, nsteps
+
                   call nek_advance()
+                  
+                  ! Set restart fields if present.
+                  if (istep <= nrst) call self%get_rst(vec_in, istep)
+
                end do
-         ! Copy the final solution to vector.
+      
+      ! Copy the final solution to vector.
                call nek2vec(vec_out, vxp, vyp, vzp, prp, tp)
+      
+      ! Compute restart fields.
+               call self%compute_rst(vec_out, nrst)
+       
             class default
-               call type_error('vec_out','nek_dvector','OUT',this_module,'exptA_rmatvec')
+               call type_error('vec_out','nek_dvector','OUT',this_module, this_procedure)
             end select
          class default
-            call type_error('vec_in','nek_dvector','IN',this_module,'exptA_rmatvec')
+            call type_error('vec_in','nek_dvector','IN',this_module, this_procedure)
          end select
+         end procedure
+
+         module procedure exptA_compute_rst
+            character(len=*), parameter :: this_procedure = 'exptA_compute_rst'
+            type(nek_dvector) :: vec_rst
+            character(len=128) :: msg
+            real(dp) :: rtmp
+            select type(vec_out)
+            type is (nek_dvector)
+               write(msg,'(A,I0,A)') 'Run ', nrst, ' extra step(s) to fill up restart arrays.'
+               call nek_log_information(msg, this_module, this_procedure)
+               fintim = fintim + nrst*dt
+               do istep = nsteps + 1, nsteps + nrst
+                  call nek_advance()
+                  call nek2vec(vec_rst, vxp, vyp, vzp, prp, tp)
+                  call vec_out%save_rst(vec_rst, istep - nsteps)
+               end do
+            class default
+               call type_error('vec_out','nek_dvector','OUT',this_module, this_procedure)
+            end select
+         end procedure
+
+         module procedure exptA_get_rst
+            character(len=*), parameter :: this_procedure = 'exptA_get_rst'
+            type(nek_dvector) :: vec_rst
+            character(len=128) :: msg
+            select type(vec_in)
+            type is (nek_dvector)
+               if (vec_in%has_rst_fields()) then
+                  call vec_in%get_rst(vec_rst, istep)
+                  call vec2nek(vxp, vyp, vzp, prp, tp, vec_rst)
+               end if
+            class default
+               call type_error('vec_in','nek_dvector','IN',this_module, this_procedure)
+            end select
          end procedure
       end submodule
--- src/linops/floquet_operator_torus.f90
+++ src/linops/floquet_operator_torus.f90
@@ -2,7 +2,7 @@
          implicit none
       contains
          module procedure floquet_init
-      ! internal
+         character(len=*), parameter :: this_procedure = 'floquet_init'
          integer :: idx, ns
          integer :: nsaver, wdsizr, ierr
          logical :: existfile
@@ -16,7 +16,7 @@
          if (existfile) then
             self%baseflow_computed = .true.
             msg = "2d baseflow files exist. No not solve for baseflow"
-            call nek_log_message(msg, module=this_module, procedure="floquet_init")
+            call nek_log_message(msg, this_module, this_procedure)
             do while (existfile)
                ! read first file and get nsteps
                call pipe%get_nsteps_from_header(fname, nsaver)
@@ -28,11 +28,11 @@
             call bcast(ns, isize)          ! broadcast number of saved snapshots
             call pipe%set_nsteps(ns)
             write(msg,'(A,I0,A,I0,A)') 'Found ', idx-1, ' baseflow files: ', ns, ' timesteps per period.'
-            call nek_log_message(msg, module=this_module, procedure="floquet_init")
+            call nek_log_message(msg, this_module, this_procedure)
          else
             self%baseflow_computed = .false.
             msg = "No 2d bseflow files found. Solve for baseflow at first iteration."
-            call nek_log_message(msg, module=this_module, procedure="floquet_init")
+            call nek_log_message(msg, this_module, this_procedure)
       ! Set the baseflow for computation
             call vec2nek(vx, vy, vz, pr, t, self%baseflow)
          end if
@@ -41,12 +41,19 @@
          end procedure floquet_init
       
          module procedure floquet_matvec
+         character(len=*), parameter :: this_procedure = 'floquet_matvec'
+         integer :: nrst
          select type (vec_in)
          type is (nek_dvector)
             select type (vec_out)
             type is (nek_dvector)
+
                if (.not. self%is_initialized) call self%init()
-               call pipe%set_2d_mode('floquet') ! reset output counter to load baseflow files in order
+               ! reset output counter to load baseflow files in order
+               call pipe%set_2d_mode('floquet')
+
+               nrst = abs(param(27)) - 1
+
       ! Ensure correct nek status
                if (.not. self%baseflow_computed) then
                   call pipe%set_save_base(.true.)
@@ -60,8 +67,10 @@
      &                                     endtime        = self%tau, 
      &                                     variable_dt    = .true.) ! -> load baseflow from 2d files
                end if
+
       ! Set the initial condition for Nek5000's linearized solver.
                call vec2nek(vxp, vyp, vzp, prp, tp, vec_in)
+
       ! Integrate the equations forward in time.
                time = 0.0_dp
                if (.not. self%baseflow_computed) then
@@ -71,36 +80,53 @@
                      call pipe%compute_bf_forcing(time) ! --> set neklab_forcing data
                      call pipe%save_2d_fields(vx,vy,vz)
                      call nek_advance()
+                     ! Set restart fields if present.
+                     if (istep <= nrst) call self%get_rst(vec_in, istep)
                   end do
                   call pipe%outpost_2d_fields()
-         ! Record the mass flow rate and number of timesteps per period for subsequent linear runs
+                  ! Record the mass flow rate and number of timesteps per period for subsequent linear runs
                   call pipe%set_nsteps(istep)
                   self%baseflow_computed = .true. ! we only need to do this once
                else
                   do istep = 1, pipe%get_nsteps()
-                     call pipe%set_baseflow(vx, vy, vz, istep) ! sets the baseflow field and the appropriate timestep
+                     ! sets the baseflow field and the appropriate timestep
+                     call pipe%set_baseflow(vx, vy, vz, istep)
                      call nek_advance()
+                     ! Set restart fields if present.
+                     if (istep <= nrst) call self%get_rst(vec_in, istep)
                   end do
                end if
+
       ! Extract the final solution to vector.
                call nek2vec(vec_out, vxp, vyp, vzp, prp, tp)
 
+      ! Compute restart fields.
+               call self%compute_rst(vec_out, nrst)
+
                self%baseflow_computed = .true. ! we only need to do this once
+            
             class default
-               call type_error('vec_out','nek_dvector','OUT',this_module,'floquet_matvec')
+               call type_error('vec_out','nek_dvector','OUT',this_module, this_procedure)
             end select
          class default
-            call type_error('vec_in','nek_dvector','IN',this_module,'floquet_matvec')
+            call type_error('vec_in','nek_dvector','IN',this_module, this_procedure)
          end select
          end procedure floquet_matvec
       
          module procedure floquet_rmatvec
+         character(len=*), parameter :: this_procedure = 'floquet_rmatvec'
+         integer :: nrst
          select type (vec_in)
          type is (nek_dvector)
             select type (vec_out)
             type is (nek_dvector)
+
                if (.not. self%is_initialized) call self%init()
-               call pipe%set_2d_mode('floquet') ! reset output counter to load baseflow files in order
+               ! reset output counter to load baseflow files in order
+               call pipe%set_2d_mode('floquet')
+
+               nrst = abs(param(27)) - 1
+
       ! Ensure correct nek status
                if (.not. self%baseflow_computed) then
                   call pipe%set_save_base(.true.)
@@ -117,8 +143,10 @@
      &                                     endtime        = self%tau,  
      &                                     variable_dt    = .true.) ! -> load baseflow from 2d files
                end if
+
       ! Set the initial condition for Nek5000's linearized solver.
                call vec2nek(vxp, vyp, vzp, prp, tp, vec_in)
+
       ! Integrate the equations forward in time.
                time = 0.0_dp
                if (.not. self%baseflow_computed) then
@@ -128,25 +156,77 @@
                      call pipe%compute_bf_forcing(time) ! --> set neklab_forcing data
                      call pipe%save_2d_fields(vx,vy,vz)
                      call nek_advance()
+                     ! Set restart fields if present.
+                     if (istep <= nrst) call self%get_rst(vec_in, istep)
                   end do
                   call pipe%outpost_2d_fields()
-         ! Record the mass flow rate and number of timesteps per period for subsequent linear runs
+                  ! Record the mass flow rate and number of timesteps per period for subsequent linear runs
                   call pipe%set_nsteps(istep)
                   self%baseflow_computed = .true. ! we only need to do this once
                else
                   do istep = 1, pipe%get_nsteps()
-                     call pipe%set_baseflow(vx, vy, vz, istep) ! sets the baseflow field and the appropriate timestep
+                     ! sets the baseflow field and the appropriate timestep
+                     call pipe%set_baseflow(vx, vy, vz, istep)
                      call nek_advance()
+                     ! Set restart fields if present.
+                     if (istep <= nrst) call self%get_rst(vec_in, istep)
                   end do
                end if
+
       ! Extract the final solution to vector.
                call nek2vec(vec_out, vxp, vyp, vzp, prp, tp)
+      
+      ! Compute restart fields.
+               call self%compute_rst(vec_out, nrst)
+
+               self%baseflow_computed = .true. ! we only need to do this once
+
             class default
-               call type_error('vec_out','nek_dvector','OUT',this_module,'floquet_rmatvec')
+               call type_error('vec_out','nek_dvector','OUT',this_module, this_procedure)
             end select
          class default
-            call type_error('vec_in','nek_dvector','IN',this_module,'floquet_rmatvec')
+            call type_error('vec_in','nek_dvector','IN',this_module, this_procedure)
          end select
          end procedure floquet_rmatvec
+
+         module procedure floquet_compute_rst
+            character(len=*), parameter :: this_procedure = 'floquet_compute_rst'
+            type(nek_dvector) :: vec_rst
+            character(len=128) :: msg
+            integer :: nstp
+            select type(vec_out)
+            type is (nek_dvector)
+               nstp = pipe%get_nsteps()
+               write(msg,'(A,I0,A)') 'Run ', nrst, ' extra step(s) to fill up restart arrays.'
+               call nek_log_information(msg, this_module, this_procedure)
+               ! reset output counter to load baseflow files in order
+               call pipe%set_2d_mode('floquet')
+               fintim = fintim + nrst*dt
+               do istep = nstp + 1, nstp + nrst
+                  ! sets the baseflow field and the appropriate timestep
+                  call pipe%set_baseflow(vx, vy, vz, istep - nstp)
+                  call nek_advance()
+                  call nek2vec(vec_rst, vxp, vyp, vzp, prp, tp)
+                  call vec_out%save_rst(vec_rst, istep - nstp)
+               end do
+            class default
+               call type_error('vec_out','nek_dvector','OUT',this_module, this_procedure)
+            end select
+         end procedure floquet_compute_rst
+
+         module procedure floquet_get_rst
+            character(len=*), parameter :: this_procedure = 'floquet_get_rst'
+            type(nek_dvector) :: vec_rst
+            character(len=128) :: msg
+            select type(vec_in)
+            type is (nek_dvector)
+               if (vec_in%has_rst_fields()) then
+                  call vec_in%get_rst(vec_rst, istep)
+                  call vec2nek(vxp, vyp, vzp, prp, tp, vec_rst)
+               end if
+            class default
+               call type_error('vec_in','nek_dvector','IN',this_module, this_procedure)
+            end select
+         end procedure floquet_get_rst
 
       end submodule
